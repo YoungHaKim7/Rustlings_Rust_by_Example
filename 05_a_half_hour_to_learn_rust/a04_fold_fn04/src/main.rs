a4beb2369a1359222665dffd839193935e67b789
김영하 <gy-gyoung@gim-yeonghas-MacBook-Pro.local>
2024-01-13 20:51:49 +0900

    Vector to Array__ fold fn 04

---
+++ 05_a_half_hour_to_learn_rust/a04_fold_fn04/src/main.rs
@@ -0,0 +1,17 @@
+use std::{convert::TryInto, error::Error};
+
+fn vector_to_array<T, const N: usize>(v: Vec<T>) -> Result<[T; N], Box<dyn Error>> {
+    Ok(v.try_into()
+        .map_err(|v: Vec<T>| format!("Expected a Vec of length {} but it was {}", N, v.len()))?)
+}
+
+fn main() {
+    let x: Vec<i32> = (1..11).collect::<Vec<_>>();
+
+    // Specify the expected array length as a constant
+    let y: [i32; 10] = vector_to_array(x).unwrap();
+    println!("{:?}", &y);
+
+    let z = y.iter().fold(0, |x, y| x + y);
+    println!("{}", z);
+}
